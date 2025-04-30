ef5d67a77543b699afa1389b40dec3d104b05941
Jeff Otterson <n1kdo@users.noreply.github.com>
2025-04-30 16:33:29 -0400

    optimizations

--- src/band_selector/alcd.py
+++ src/band_selector/alcd.py
@@ -1,4 +1,13 @@
 # LCD class for Micropython and uasyncio.
+#
+__author__ = 'J. B. Otterson'
+__copyright__ = 'Copyright 2024, 2025 J. B. Otterson N1KDO.'
+__version__ = '0.1.2'
+#
+# bastardized from Peter Hinch's alcd.py retrieved from
+#  https://github.com/peterhinch/micropython-async/blob/master/v3/as_drivers/hd44780/alcd.py
+#
+# LCD class for Micropython and uasyncio.
 # Author: Peter Hinch
 # Copyright Peter Hinch 2017 Released under the MIT license
 # V1.1 24 Apr 2020 Updated for uasyncio V3
@@ -15,8 +24,10 @@
 # Author : Matt Hawkins
 # Site   : http://www.raspberrypi-spy.co.uk
 
-import time
 import asyncio
+
+# noinspection PyUnresolvedReferences
+from time import (sleep_ms, sleep_us)
 
 # noinspection PyUnresolvedReferences
 from machine import Pin
@@ -50,62 +61,66 @@ PINLIST = ("Y1", "Y2", "Y6", "Y5", "Y4", "Y3")  # As used in testing.
 
 
 class LCD:  # LCD objects appear as read/write lists
+    __slots__ = ('_LCD_E', '_LCD_RS', '_datapins', '_cols', '_rows', '_lines', '_dirty', '_initialising')
+
     INITSTRING = b"\x33\x32\x28\x0C\x06\x01"
-    LCD_LINES = b"\x80\xC0"  # LCD RAM address for the 1st and 2nd line (0 and 40H)
+    LCD_LINES = (0x80, 0xC0)  # LCD RAM address for the 1st and 2nd line (0 and 40H)
     CHR = True
     CMD = False
-    E_PULSE = 50  # Timing constants in uS
-    E_DELAY = 50
+    E_PULSE = const(50)  # Timing constants in uS
+    E_DELAY = const(50)
 
     def __init__(self, pinlist, cols, rows=2):  # Init with pin nos for enable, rs, D4, D5, D6, D7
-        self.initialising = True
-        self.LCD_E = Pin(pinlist[1], Pin.OUT)  # Create and initialise the hardware pins
-        self.LCD_RS = Pin(pinlist[0], Pin.OUT)
-        self.datapins = [Pin(pin_name, Pin.OUT) for pin_name in pinlist[2:]]
-        self.cols = cols
-        self.rows = rows
-        self.lines = [""] * self.rows
-        self.dirty = [False] * self.rows
-        for thisbyte in LCD.INITSTRING:
-            self.lcd_byte(thisbyte, LCD.CMD)
-            self.initialising = False  # Long delay after first byte only
-        asyncio.create_task(self.runlcd())
+        self._initialising = True
+        self._LCD_E = Pin(pinlist[1], Pin.OUT)  # Create and initialise the hardware pins
+        self._LCD_RS = Pin(pinlist[0], Pin.OUT)
+        self._datapins = [Pin(pin_name, Pin.OUT) for pin_name in pinlist[2:]]
+        self._cols = cols
+        self._rows = rows
+        self._lines = [""] * self._rows
+        self._dirty = [False] * self._rows
+        for b in LCD.INITSTRING:
+            self.lcd_byte(b, LCD.CMD)
+            self._initialising = False  # Long delay after first byte only
+        asyncio.create_task(self.update_lcd())
 
     def lcd_nybble(self, bits):  # send the LS 4 bits
-        for pin in self.datapins:
+        for pin in self._datapins:
             pin.value(bits & 0x01)
             bits >>= 1
-        time.sleep_us(LCD.E_DELAY)  # 50μs
-        self.LCD_E.value(True)  # Toggle the enable pin
-        time.sleep_us(LCD.E_PULSE)
-        self.LCD_E.value(False)
-        if self.initialising:
-            time.sleep_ms(5)
+        sleep_us(LCD.E_DELAY)  # 50μs
+        self._LCD_E.value(True)  # Toggle the enable pin
+        sleep_us(LCD.E_PULSE)
+        self._LCD_E.value(False)
+        if self._initialising:
+            sleep_ms(5)
         else:
-            time.sleep_us(LCD.E_DELAY)  # 50μs
+            sleep_us(LCD.E_DELAY)  # 50μs
 
     def lcd_byte(self, bits, mode):  # Send byte to data pins: bits = data
-        self.LCD_RS.value(mode)  # mode = True  for character, False for command
+        self._LCD_RS.value(mode)  # mode = True  for character, False for command
         self.lcd_nybble(bits >> 4)  # send high bits
         self.lcd_nybble(bits)  # then low ones
 
     def __setitem__(self, line, message):  # Send string to display line 0 or 1
-        message = "{0:{1}.{1}}".format(message, self.cols)
-        if message != self.lines[line]:  # Only update LCD if data has changed
-            self.lines[line] = message  # Update stored line
-            self.dirty[line] = True  # Flag its non-correspondence with the LCD device
+        message = "{0:{1}.{1}}".format(message, self._cols)
+        if message != self._lines[line]:  # Only update LCD if data has changed
+            self._lines[line] = message  # Update stored line
+            self._dirty[line] = True  # Flag its non-correspondence with the LCD device
 
     def __getitem__(self, line):
-        return self.lines[line]
+        return self._lines[line]
 
-    async def runlcd(self):  # Periodically check for changed text and update LCD if so
+    async def update_lcd(self):
+        asleep_ms = asyncio.sleep_ms
+        # Periodically check for changed text and update LCD if so
         while True:
-            for row in range(self.rows):
-                if self.dirty[row]:
+            for row in range(self._rows):
+                if self._dirty[row]:
                     msg = self[row]
-                    self.dirty[row] = False
+                    self._dirty[row] = False
                     self.lcd_byte(LCD.LCD_LINES[row], LCD.CMD)
                     for thisbyte in msg:
                         self.lcd_byte(ord(thisbyte), LCD.CHR)
-                        await asyncio.sleep_ms(0)  # Reschedule ASAP
-                await asyncio.sleep_ms(20)  # Give other coros a look-in
+                        await asleep_ms(0)  # Reschedule ASAP
+            await asleep_ms(20)  # Give other coros a look-in
